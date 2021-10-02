ef1806576b1e3396723737c67e196a1aa110409e
Shraddha Vinayak Dambalkar <47273861+Shraddha3696@users.noreply.github.com>
2021-10-02 22:29:52 +0530

    Update main.js

--- Demo_10_KO/js/main.js
+++ Demo_10_KO/js/main.js
@@ -1,28 +1,33 @@
-$(document).ready(function(){  
+function MyViewModel() {
+    fname = ko.observable("");
+    lname = ko.observable("");
 
 
-$("#jsonbtn").click(function(){
-    const firstName=$("#firstName").val();
-    const lastName=$("#lastName").val();
-    console.log(firstName);
-    const person={firstName:firstName,lastName:lastName};
-    console.log(person);
-    const jsoString=JSON.stringify(person);
-    const root =$("#root");
-    root.html("The JSON Data is ");
-    root.append("<h3 id='h3'</h3>");  
-    const h3 = $("#h3");
-    h3.html(jsoString);  
-    $("#firstName").val("");
-    $("#lastName").val("");
-});
-$("#jsbtn").click(function(){
-    const h3=$("#h3");
-    const jsonString=h3.text();
-    console.log(jsonString);
-    const PERSON=JSON.parse(jsonString);
-    firstName.value=PERSON.firstName;
-    lastName.value=PERSON.lastName;
-    h3.text("");
-});
-});
+    self.onSubmit = function () {
+        const firstName = fname();
+        const lastName = lname();
+        console.log(firstName);
+        const person = { firstName: firstName, lastName: lastName };
+        console.log(person);
+        const jsoString = JSON.stringify(person);
+        const root = $("#root");
+        root.html("The JSON Data is ");
+        root.append("<h3 id='h3'</h3>");
+        const h3 = $("#h3");
+        h3.html(jsoString);
+        fname("");
+        lname("");
+    }
+    self.onJSObject = function () {
+        const h3 = $("#h3");
+        const jsonString = h3.text();
+        console.log(jsonString);
+        const PERSON = JSON.parse(jsonString);
+        fname(PERSON.firstName);
+        lname(PERSON.lastName);
+        h3.text("");
+    }
+}
+
+const vm = new MyViewModel();
+ko.applyBindings(vm);
