1201d521001290bc20b439e1b94c574eed9141bf
Shraddha Vinayak Dambalkar <47273861+Shraddha3696@users.noreply.github.com>
2021-10-02 22:21:14 +0530

    Add files via upload

--- Add_Hobby_JQuery/js/main.js
+++ Add_Hobby_JQuery/js/main.js
@@ -1,1 +1,11 @@
+const root = $("#root");
+const ul = root.append("<ul></ul>");
+
+function addHobby() {   
+
+    const item = $("#hobby").val();
+    $("#hobby").val("");
+     ul.append("<li>"+item+"</li>");
+
+}
 
